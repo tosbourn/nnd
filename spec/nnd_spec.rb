fad3af14f47a9b3b8b0ceb53521f5a0281d73788
tosbourn <toby.osbourn@googlemail.com>
2015-12-06 15:18:07 +0000

    Writes out main specs

---
+++ spec/nnd_spec.rb
@@ -0,0 +1,19 @@
+require './nnd'
+
+describe Nnd do
+  describe 'input' do
+    it 'expects two bits of input'
+    it 'accepts non-zero integers as input'
+  end
+
+  describe 'processing' do
+    it 'will correctly calculate the value'
+  end
+
+  describe 'output' do
+    it 'will show correct answer'
+    it 'will show both inputs'
+    it 'will show first step to work out base'
+    it 'will show second step to work simple multiplication'
+  end
+end
